6224e698eb4fca860adaeed67cdca882b10237eb
GproSoftware828 <JetSendProductions@Gmail.com>
2019-09-16 15:36:33 +1000

    Cookie WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
 
   get 'chatroom', to: 'rooms#show'  
-  root 'pages#index'
+  root to: 'static#home'
   mount ActionCable.server => '/cable'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
