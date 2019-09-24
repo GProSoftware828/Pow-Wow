3e49929aca33b0f218c203c1845c7bdb11f11727
GproSoftware828 <JetSendProductions@Gmail.com>
2019-09-24 10:35:08 +1000

    Building Reg comp WIP

--- app/javascript/components/auth/Registration.js
+++ app/javascript/components/auth/Registration.js
@@ -1,7 +1,65 @@
 import React, { Component } from 'react';
 
 export default class Registration extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+      password_confirmation: '',
+      registrationErrors: ''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log('form submitted');
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   render() {
-    return <div>Registration goes here</div>;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            required
+          ></input>
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
+          ></input>
+
+          <input
+            type="password"
+            name="password_confirmation"
+            placeholder="Password confirmation"
+            value={this.state.password_confirmation}
+            onChange={this.handleChange}
+            required
+          ></input>
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    );
   }
 }
