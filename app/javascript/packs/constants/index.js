2ae404a64c00ec9f870de1bd6efbe97f500a1911
GproSoftware828 <JetSendProductions@Gmail.com>
2019-10-06 16:12:01 +1100

    Built out chat features front end WIP- registerserviceworker error

---
+++ app/javascript/packs/constants/index.js
@@ -0,0 +1,6 @@
+export const API_ROOT = 'http://localhost:2999';
+export const API_WS_ROOT = 'ws://localhost:2999/cable';
+export const HEADERS = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json'
+};
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ActionCableProvider } from 'react-actioncable-provider';
 import PropTypes from 'prop-types';
 import App from '../components/App';
+import registerServiceWorker from 'registerServiceWorker';
+import { API_WS_ROOT } from './constants';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <App />,
+    <ActionCableProvider url={API_WS_ROOT}>
+      <App />
+    </ActionCableProvider>,
+    document.getElementById('root'),
     document.body.appendChild(document.createElement('div'))
   );
 });
+registerServiceWorker();
