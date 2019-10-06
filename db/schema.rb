5ceba657873852fb7f52a1c7b023f5bbdc4a2990
GproSoftware828 <JetSendProductions@Gmail.com>
2019-10-06 19:33:23 +1100

    Updated database for integration

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_020303) do
+ActiveRecord::Schema.define(version: 2019_10_06_082826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "messages", force: :cascade do |t|
-    t.text "content"
+  create_table "conversations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "content"
   end
 
   create_table "users", force: :cascade do |t|
